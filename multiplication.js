1edd74a624dbf2091e973aa4fe943182831459a2
Mamoudou12 <bmamoudou463@gmail.com>
2024-04-28 16:48:59 +0000

    possibilité à l'utilisateur de saisir 3 nombre

--- multiplication.js
+++ multiplication.js
@@ -4,5 +4,8 @@ function calculate(event) {
     let num1 = parseFloat(document.getElementById('num1').value);
     let num2 = parseFloat(document.getElementById('num2').value);
     let result = num1 * num2;
+    if (isNaN(num3)) {
+        num3 = 0; 
+      }
     document.getElementById('result').innerText = 'Résultat : ' + result;
   }
